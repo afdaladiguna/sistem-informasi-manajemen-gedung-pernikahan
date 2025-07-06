588c4c1daf2c031528ab586d559e4f1f631f7405
Muhammad Afdal Adiguna <m.afdal.adiguna@gmail.com>
2025-07-06 20:33:43 +0700

    update route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
+const { isAuthenticated } = require("../middleware/authMiddleware");
 
 // Rute Halaman Utama - Menampilkan semua gedung
-router.get("/", userController.getAllGedung);
+router.get("/", isAuthenticated, userController.getAllGedung);
 
-router.get("/dashboard", authController.getDashboard);
+router.get("/dashboard", isAuthenticated, authController.getDashboard);
 // Rute Halaman Detail Gedung
-router.get("/gedung/:id", userController.getGedungDetail);
+router.get("/gedung/:id", isAuthenticated, userController.getGedungDetail);
 
 module.exports = router;
